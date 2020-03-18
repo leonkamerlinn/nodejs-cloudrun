a17efabf752bf88a93cce08f86ea85059771486c
Leon <leon.kamerlin@dings.solutions>
2020-03-18 19:27:03 +0100

    radi

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
 app.get('/', async (req, res, next) => {
-  res.status(200).send('TEST TEST 222')
+  res.status(200).send('RADI!!')
 })
 app.listen(8080, () => console.log('Server is running on port 8080'))
