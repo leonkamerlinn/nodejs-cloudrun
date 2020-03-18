ef758650668b90734ffa9cdaff8da6661cbd487a
Leon <leon.kamerlin@dings.solutions>
2020-03-18 16:09:15 +0100

    update app

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
 app.get('/', async (req, res, next) => {
-  res.status(200).send('Hello World!')
+  res.status(200).send('Hello World! Kako si?')
 })
 app.listen(8080, () => console.log('Server is running on port 8080'))
